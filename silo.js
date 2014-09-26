2238e96632a421ed17deb142c4cc93182d6b7429
Subhash A <s.a@sap.com>
2014-09-26 21:12:56 +0530

    changed the require and added commit method structure

--- silo.js
+++ silo.js
@@ -1,6 +1,7 @@
 var rl = require("readline");
-var df = require("./difftext.js");
+var df = require("./core/difftext.js");
 var fsys = require("fs");
+
 intf = rl.createInterface({
     "input":process.stdin,
     "output":process.stdout
@@ -12,6 +13,13 @@ var createChangeLogDirectory = function(callback) {
     fsys.mkdir(currdir+dirname,callback);
 };
 
+var commitChanges = function (callback) {
+    var afterReadDirectory = function (err, files) {
+	console.log(files);
+	callback();
+    };
+    var files = fsys.readdir("./",afterReadDirectory);
+};
 intf.setPrompt("~ ",2);
 intf.prompt(true);
 intf.on("line", function(cmd){
@@ -19,15 +27,19 @@ intf.on("line", function(cmd){
 	case "exit": intf.close();
 	break;
 	case "commit": console.log("This is a commit command");
-	intf.prompt(true);
+	var changesCommited = function() {
+	    intf.prompt(true);
+	};
+	commitChanges(changesCommited);
 	break;
-	case "init": console.log("This is a commit command");
+	case "init": console.log("This is a init command");
 	var dirCreated = function () {
+	    console.log("directory is initialized with silo");
 	    intf.prompt(true);
 	};
 	createChangeLogDirectory(dirCreated);
 	break;
-	case "revert": console.log("This is a commit command");
+	case "revert": console.log("This is a revert command");
 	intf.prompt(true);
 	break;
 	default: console.log(
